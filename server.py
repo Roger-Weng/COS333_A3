c8da2849c7af195d2542f39b796127e54036f79c
VishvaIlavelan <vi6908@princeton.edu>
2024-03-13 18:50:00 -0700

    first commit

--- server.py
+++ server.py
@@ -1,10 +1,30 @@
 
 import flask
+import dbconnect
 
 app = flask.Flask(__name__, template_folder=".")
 
 @app.route("/", methods=["GET"])
 def index():
+    dept = flask.request.args.get("dept")
+    number= flask.request.args.get("number")
+    area = flask.request.args.get("area")
+    title = flask.request.args.get("title")
+    if dept is None:
+        dept = ''
+    if number is None:
+        number = ''
+    if area is None:
+        area = ''
+    if title is None:
+        title = ''
+
+
+    query_results = dbconnect.search(dept, number, area, title)
+
     html_code = flask.render_template('index.html')
     response = flask.make_response(html_code)
     return response
+
+
+
