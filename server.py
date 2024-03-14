f2d3fc1c7b9ba9b0db334fd6875626c57170f6e1
VishvaIlavelan <vi6908@princeton.edu>
2024-03-13 18:16:03 -0700

    first commit

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
 
+import flask
+
+app = flask.Flask(__name__, template_folder=".")
+
+@app.route("/", methods=["GET"])
+def index():
+    html_code = flask.render_template('index.html')
+    response = flask.make_response(html_code)
+    return response
